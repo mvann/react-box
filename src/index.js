6f9cb630428b55f6f0184475acfa9627a3747a37
Michael Vann <mvann@e1z2r3p39.42.us.org>
2017-12-04 11:14:05 -0800

    fixed index

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import Box from './Box';
 import './index.css';
 
 ReactDOM.render(
-	let boxes = [['a'],['b'],['c']];
-	<Box content={boxes} />,
+	<Box content={[['a'],['b'],['c']]} />,
 	document.getElementById('root')
 );
